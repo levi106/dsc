fcd512d2efe5eb23aa3afc1f10bb18af161eb7df
levi106 <16788901+levi106@users.noreply.github.com>
2022-10-18 11:52:02 +0900

    update DSC

---
+++ sofs/prep/PrepareClusterNode.ps1
@@ -0,0 +1,87 @@
+#
+# Copyright 2020 Microsoft Corporation. All rights reserved."
+#
+
+configuration PrepareClusterNode
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$AdminCreds
+    )
+
+    try 
+    {
+        Get-ItemPropertyValue 'HKLM:\SOFTWARE\Microsoft\Windows Azure' -Name dscPreboot
+    } 
+    catch 
+    {
+        Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows Azure' -Name dscPreboot -Value $True
+        Start-Sleep -Seconds 30
+        Restart-Computer -Force
+    }
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, ComputerManagementDsc, ActiveDirectoryDsc
+
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("$($Admincreds.UserName)@${DomainName}", $Admincreds.Password)
+   
+    Node localhost
+    {
+
+        WindowsFeature FC
+        {
+            Name = "Failover-Clustering"
+            Ensure = "Present"
+        }
+
+        WindowsFeature FCPS
+        {
+            Name = "RSAT-Clustering-PowerShell"
+            Ensure = "Present"
+        }
+
+        WindowsFeature FCCmd {
+            Name      = "RSAT-Clustering-CmdInterface"
+            Ensure    = "Present"
+        }
+
+        WindowsFeature ADPS
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+        }
+
+        WindowsFeature FS
+        {
+            Name = "FS-FileServer"
+            Ensure = "Present"
+        }
+
+        WaitForADDomain DscForestWait 
+        { 
+            DomainName = $DomainName 
+            Credential= $DomainCreds
+            WaitForValidCredentials = $True
+            WaitTimeout = 600
+            RestartCount = 3
+            DependsOn = "[WindowsFeature]ADPS"
+        }
+
+        Computer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $DomainName
+            Credential = $DomainCreds
+            DependsOn = "[WaitForADDomain]DscForestWait"
+        }
+
+        LocalConfigurationManager 
+        {
+            RebootNodeIfNeeded = $True
+        }
+
+    }
+}

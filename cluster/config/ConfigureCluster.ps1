87d616f2a34a89af405705c71c4677aafea915ba
levi106 <16788901+levi106@users.noreply.github.com>
2022-11-10 19:07:48 +0900

    fix dependency

--- cluster/config/ConfigureCluster.ps1
+++ cluster/config/ConfigureCluster.ps1
@@ -124,10 +124,10 @@ configuration ConfigureCluster
         }
 
         Script ClusterWitness {
-            SetScript  = "if ('${WitnessType}' -eq 'Cloud') { Set-ClusterQuorum -CloudWitness -AccountName ${WitnessStorageName} -AccessKey $($WitnessStorageKey.GetNetworkCredential().Password) } else { Set-ClusterQuorum -DiskWitness `$((Get-ClusterGroup -Name 'Available Storage' | Get-ClusterResource | ? ResourceType -eq 'Physical Disk' | Sort-Object Name | Select-Object -Last 1).Name) }"
+            SetScript  = "Set-ClusterQuorum -CloudWitness -AccountName ${WitnessStorageName} -AccessKey $($WitnessStorageKey.GetNetworkCredential().Password)"
             TestScript = "((Get-ClusterQuorum).QuorumResource).Count -gt 0"
             GetScript  = "@{Ensure = if (((Get-ClusterQuorum).QuorumResource).Count -gt 0) {'Present'} else {'Absent'}}"
-            DependsOn  = "[Script]AddClusterDisks"
+            DependsOn  = "[Script]CreateCluster"
         }
 
         Script IncreaseClusterTimeouts {
